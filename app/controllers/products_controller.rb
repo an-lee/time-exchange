6def0562120fd6f3c1aa6c0ff6f1eae1ba227631
meiputi <meiputi@gmail.com>
2017-02-22 23:08:10 +0800

    implement post create function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    @posts = @product.posts
   end
 
   def add_to_cart
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  validates :content, presence: true
 end
