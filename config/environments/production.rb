5b63baf8ea44635dde01063e785e4e84f95ac748
An Li <anli@Ans-Mac-mini.local>
2017-02-26 11:55:45 +0800

    sendcloud for time-ex.heroku.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'test4timex.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'time-ex.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
